ff145c3ca8954eedf90adfa2d7329649d757762a
MartinDeep13 <martin0013esp@hotmail.com>
2020-07-31 11:51:32 -0500

    first commit

--- src/io.js
+++ src/io.js
@@ -2,51 +2,30 @@ const jsonwebtoken = require('jsonwebtoken');
 const _ = require('lodash');
 const profile = require('./controllers/profile');
 
-let connectedUsers = {};
+let users = [];
 
 module.exports = io => {
   io.on('connection', async socket => {
 
-    //get the data from jwt
+      console.log('a user connected');
+      socket.on('disconnect', function() {
+        console.log('user disconnected');
+      });
      
 
-    // use the user id as room
-    //const user = await profile.info(id);
-
-    /*io.to(email).emit('connected', { id: socket.id }); // emit when the user is connected
-    socket.broadcast.emit('joined', {
-      email,
-      email
-    });*/
-
-    //connectedUsers[id] = user;
-
-    socket.on('send', message => {
-      console.log(message);
-      var room = message['sala'];
-      console.log(room);
-      io.sockets.to(room).emit('new-message', {
-        message 
+      socket.on('message', function(data){
+        let index = users.findIndex(user => {
+          return user.userId === data.userId
+        });
+        users[index].socket.emit('message', data.message);
       });
-    });
 
-    socket.on('join', room => {
-      socket.join(room); // use the user id as room
-    });
-
-    socket.on('leave', room => {
-      socket.leave(room); // use the user id as room
-    });
-
-    /*socket.on('send-file', ({ type, url }) => {
-      socket.broadcast.emit('new-file', {
-        from: { id, username: user.username },
-        file: { type, url }
+      socket.on('online', function(userId){
+        users.push({
+          socket,
+          userId
+        })
+        console.log(users)
       });
-    });*/
-
-    /*socket.on('disconnect', () => {
-      io.emit('disconnected', email);
-    });*/
   });
 }; 
