2291143f7c2ada11eb6d31f2cb3b4cdae468fc2b
root <root@ip-172-31-57-227.ec2.internal>
2020-08-13 23:32:05 +0000

    cambios

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+  host: process.env.DB_PORT,
   dialect: process.env.DB_CONNECTION /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
 });
 async function test(){
