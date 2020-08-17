f0dd1ec6f3229a112f5ca0b71f369e3799c65e97
DaniGadget18 <dani_181298@hotmail.com>
2020-08-17 13:10:56 -0500

    cors

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-app.use(cors({origin: '*'}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set('view engine', 'ejs');
 
 // public files
